dc452b0c84db842a92af654e67affa86c2b75ed0
Franco Vargas <francovargas25@gmail.com>
2018-05-14 23:54:38 -0500

    to be completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  state={
+    title : this.props.title,
+    image : this.props.image,
+    description : this.props.description,
+    pelicula:[]
+  }
+
+componentWillMount(){
+  fetch('https://api.themoviedb.org/3/genre/28/movies?api_key=86ce2b6f3a7348ed983ffde1e0e524f1&language=es&include_adult=false&sort_by=created_at.asc')
+  .then(r=>r.json())
+  .then(pelicula=>{
+    this.setState({pelicula:pelicula.results})
+  })
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <a href="#" onClick>Action</a>
+         <a href="#" onClick>Comedy</a>
+         <a href="#" onClick>Drama</a>
+         <div className="movies">
+         {this.state.pelicula.map(m=><h2>{m.original_title + m.overview}</h2>)}
+         {this.state.pelicula.map(m=> <img src='https://image.tmdb.org/t/p/w500+{m.poster_path}'> </img>)}
+         </div>
       </div>
     );
   }
